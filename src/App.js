95ff26774bceab6fc264c8c7044251d30f037b2f
Rich Garrick <richg341@gmail.com>
2018-05-17 13:47:34 -0700

    start firebase integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Router from './components/Router/index.js';
 import NavBar from './components/NavBar/index.js';
 import './App.css';
+import firebase from 'firebase';
+
 
 class App extends Component {
   render() {
--- src/components/Battlemechs/index.js
+++ src/components/Battlemechs/index.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import './styles.css';
+import firebase from 'firebase';
 
 class Battlemechs extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: "test"
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('battletech-1');
+    const nameRef = rootRef.child('ChassisID');
+    nameRef.on('value', snap => {
+      this.setState({
+        name: snap.val()
+      });
+    });
+  }
+
   render() {
+    
     return(
       <section className="main">
         <div>
           <h1>This is: Battlemechs Component</h1>
+        </div>
+        <div>
+          <h2>3025</h2>
+          <select>
+            <option value="allMechs">All BattleMechs</option>
+            <option value="lightMechs">Light</option>
+            <option value="mediumMechs">Medium</option>
+            <option value="heavyMechs">Heavy</option>
+            <option value="assaultMechs">Assault</option>
+          </select> 
+        </div>
+        <div>
+          <h1>{this.state.speed}</h1>
         </div>
       </section>
     );
